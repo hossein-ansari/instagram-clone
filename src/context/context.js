3a81666189b9c9d545faa32fed5f369693f71bcc
hossein <hossein838485@gmail.com>
2023-08-22 12:43:46 +0330

    create project

---
+++ src/context/context.js
@@ -0,0 +1,14 @@
+import React, { createContext, useEffect, useState } from "react";
+const contextBox = createContext();
+const AllDatasProvider = ({ children }) => {
+  return (
+   <contextBox.Provider
+   value={
+
+   }
+   >
+    {children}
+   </contextBox.Provider>
+  )
+}
+export { AllDatasProvider, contextBox };
---
+++ src/routes/routes.js
@@ -0,0 +1,4 @@
+import App from "../App";
+let Routes = [
+];
+export default Routes;
