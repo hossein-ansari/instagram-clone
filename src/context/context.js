be0d3a83d16e0c54de4462fbfb4ed4d11bc75209
hossein <hossein838485@gmail.com>
2023-08-31 10:14:49 +0330

    change project

--- src/context/context.js
+++ src/context/context.js
@@ -1,46 +1,11 @@
 import React, { createContext, useEffect, useState } from "react";
 const contextBox = createContext();
 const AllDatasProvider = ({ children }) => {
-  // datas
-  const [storiesImg, setStoriesIMg] = useState();
-  const [exploreImg, setExploreIMg] = useState();
-  // user account
-  const [userAccount, setUserAccount] = useState();
-  const [canShowStory, setCanShowStory] = useState(false);
-  // local storage
-  const storage = window.localStorage
-  const shuffleArray = (array) => {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-  };
-  useEffect(() => {
-    fetch(
-      "https://api.unsplash.com/search/photos?page=1&query=office&client_id=cMPwP5FZjsJChp5jVLcHCg2TT5jjzws017cudCbpRlQ"
-    )
-      .then((response) => response.json())
-      .then((data) => setStoriesIMg(shuffleArray(data.results)));
-    fetch(
-      "https://api.unsplash.com/search/photos?page=1&query=office&client_id=cMPwP5FZjsJChp5jVLcHCg2TT5jjzws017cudCbpRlQ&per_page=50"
-    )
-      .then((response) => response.json())
-      .then((data) => setExploreIMg(shuffleArray(data.results)));
 
-  }, []);
   return (
     <contextBox.Provider
       value={{
-        storiesImg,
-        canShowStory,
-        exploreImg,
-        userAccount,
-        storage,
-        setUserAccount,
-        setStoriesIMg,
-        setCanShowStory,
-        setExploreIMg,
+
       }}
     >
       {children}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,17 +1,8 @@
 import Home from "../component/Home";
-import Explore from "../component/Explore";
-import Profile from '../component/Profile'
-import StoryScreen from '../component/StoryScreen'
-import Post from "../component/Post";
-import LoginUser from "../component/LoginUser";
+
 let Routes = [
   { path: "/", element: <Home /> },
-  { path: "/explore", element: <Explore /> },
-  { path: "/profile", element: <Profile /> },
-  { path: "/story-screen/:storyId", element: <StoryScreen /> },
-  { path: "/post/:id", element: <Post /> },
-  { path: "/login", element: <LoginUser /> },
-  { path: "/register", element: <LoginUser /> },
+
 
   
 
