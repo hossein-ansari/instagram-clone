34d94820c924bbaff4d8a412c74964272ddf4ef3
hossein <hossein838485@gmail.com>
2023-09-02 18:27:07 +0330

    fetching api

--- src/context/context.js
+++ src/context/context.js
@@ -1,15 +1,21 @@
 import React, { createContext, useEffect, useState } from "react";
 const contextBox = createContext();
 const AllDatasProvider = ({ children }) => {
-
-  return (
-    <contextBox.Provider
-      value={{
-
-      }}
-    >
-      {children}
-    </contextBox.Provider>
-  );
+  useEffect(() => {
+    fetch('https://wft-geo-db.p.rapidapi.com/v1/geo/countries', {
+      method: "GET",
+      headers: {
+        'X-RapidAPI-Key': '48d15888fbmsh245d3f7b35e52f9p12d515jsndd317c5231ff',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+      }
+    })
+      .then((response) => {
+        response.text();
+      })
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
+  return <contextBox.Provider value={{}}>{children}</contextBox.Provider>;
 };
 export { AllDatasProvider, contextBox };
