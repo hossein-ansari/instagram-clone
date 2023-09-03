18b31bfd41e39404f4808372953b7d519c5084c4
hossein <hossein838485@gmail.com>
2023-09-03 13:11:24 +0330

    fetching days data

--- src/component/NavWeatherDays.js
+++ src/component/NavWeatherDays.js
@@ -1,59 +1,38 @@
-import React from "react";
+import React, { useContext } from "react";
 import "../style/navWeatherDays.css";
+import { contextBox } from "../context/context";
+import ConvertTemperature from "../custom Hooks/ConvertTemperature";
 export default function NavWeatherDays() {
+  const data = useContext(contextBox);
+  let countDays = 3;
+  function splitInfo(time) {
+    return time.split(" ")[0].slice(5);
+  }
   return (
     <div className="nav-weather-days">
       <h2>7-day forecast</h2>
       <div className="days-forecast-nav">
-        <div className="days-forecast-days">
-          <p className="days-forecast-day">today</p>
-          <p className="days-forecast-situation">
-            <i></i>sunny
-          </p>
-          <p className="days-forecast-deduction">36/22</p>
-        </div>
-        <div className="days-forecast-days">
-          <p className="days-forecast-day">today</p>
-          <p className="days-forecast-situation">
-            <i></i>sunny
-          </p>
-          <p className="days-forecast-deduction">36/22</p>
-        </div>
-        <div className="days-forecast-days">
-          <p className="days-forecast-day">today</p>
-          <p className="days-forecast-situation">
-            <i></i>sunny
-          </p>
-          <p className="days-forecast-deduction">36/22</p>
-        </div>
-        <div className="days-forecast-days">
-          <p className="days-forecast-day">today</p>
-          <p className="days-forecast-situation">
-            <i></i>sunny
-          </p>
-          <p className="days-forecast-deduction">36/22</p>
-        </div>
-        <div className="days-forecast-days">
-            <p className="days-forecast-day">today</p>
-            <p className="days-forecast-situation">
-              <i></i>sunny
-            </p>
-            <p className="days-forecast-deduction">36/22</p>
-          </div>
-          <div className="days-forecast-days">
-            <p className="days-forecast-day">today</p>
-            <p className="days-forecast-situation">
-              <i></i>sunny
-            </p>
-            <p className="days-forecast-deduction">36/22</p>
-          </div>
-          <div className="days-forecast-days last">
-            <p className="days-forecast-day">today</p>
-            <p className="days-forecast-situation">
-              <i></i>sunny
-            </p>
-            <p className="days-forecast-deduction">36/22</p>
-          </div>
+        {data.forecastInfo !== undefined
+          ? data.forecastInfo.map((days) => {
+              countDays += 1;
+              if (countDays % 8 === 0) {
+                return (
+                  <div className="days-forecast-days">
+                    <p className="days-forecast-day">
+                      {splitInfo(days.dt_txt)}
+                    </p>
+                    <p className="days-forecast-situation">
+                      <i></i>
+                      {days.weather[0].main}
+                    </p>
+                    <p className="days-forecast-deduction">
+                      {ConvertTemperature(data.forecastInfo[1].main.temp)}°
+                    </p>
+                  </div>
+                );
+              }
+            })
+          : ""}
       </div>
     </div>
   );
